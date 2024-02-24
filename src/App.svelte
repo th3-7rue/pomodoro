34a84c9926c2562e4045a57f1615531c41111834
th3-7rue <riccardorasori2005@gmail.com>
2024-02-24 19:03:30 +0100

    d

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,11 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossOrigin="anonymous"
+  />
   <link
     href="https://fonts.googleapis.com/css2?family=Salsa&display=swap"
     rel="stylesheet"
