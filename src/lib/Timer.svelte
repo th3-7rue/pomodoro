f166be510ec86b375e8b54ed78b4cf38067c8192
th3-7rue <riccardorasori2005@gmail.com>
2024-02-25 22:34:18 +0100

    h

--- src/lib/Timer.svelte
+++ src/lib/Timer.svelte
@@ -104,13 +104,13 @@
   <div class="flex justify-center items-center">
     {#if pomodoroTime < POMODORO_S / 2}
       <img
-        style="width: 50vh; height: auto;"
+        style="width: 45vh; height: auto;"
         src={pomodoroHalf}
         alt="Pomodoro a metà"
       />
     {:else}
       <img
-        style="width: 50vh; height: auto;"
+        style="width: 45vh; height: auto;"
         src={pomodoroPieno}
         alt="Pomodoro pieno"
       />
